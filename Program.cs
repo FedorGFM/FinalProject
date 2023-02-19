15d4199b9e598a26903bc296819f6f2e62ca977a
Fedor <4ips228@gmail.com>
2023-02-20 01:23:11 +0700

    Написали программу решения задачи.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿System.Console.Write("Введите количество элементов массива: ");
+int countElements = Convert.ToInt32(Console.ReadLine());
+
+string[] stringArray = new string[countElements];
+for (int i = 0; i < stringArray.Length; i++)
+{
+    Console.Write($"Введите элемент массива {i}: ");
+    stringArray[i] = Console.ReadLine();
+}
+
+string[] stringArrEll = StringArrayElements(stringArray);
+
+PrintArray(stringArrEll);
+
+string[] StringArrayElements(string[] arr)
+{
+    int index = 0;
+    string[] stringArr = new string[arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            stringArr[index] = arr[i];
+            index++;
+        }
+    }
+
+    string[] resultArr = new string[index];
+    for (int i = 0; i < resultArr.GetLength(0); i++)
+    {
+        resultArr[i] = stringArr[i];
+    }
+    return resultArr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
